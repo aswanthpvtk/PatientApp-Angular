34da95e9b5841569eadf6fb65f0db75961d0304a
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-07 06:19:05 +0530

    pat

---
+++ src/app/add-patient/add-patient.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-patient',
+  templateUrl: './add-patient.component.html',
+  styleUrls: ['./add-patient.component.css']
+})
+export class AddPatientComponent {
+
+  name=""
+  address=""
+  mob=""
+  doctor=""
+  date=""
+
+  // constructor(private api:ApiService){}
+
+  readValues=()=>
+  {
+    let data:any={"name":this.name,"address":this.address,"mob":this.mob,"date":this.date,"doctor":this.doctor}
+    console.log(data)
+    // this.api.addCourse(data).subscribe(
+    //   (response:any)=>
+    //   {
+    //     console.log(response)
+    //     if (response.status == "success") {
+
+    //       alert("Successfully added")
+  
+    //       this.name=""
+  
+    //       this.address=""
+  
+    //       this.mob=""
+  
+    //       this.date=""
+  
+    //       this.doctor=""
+  
+    //     } else {
+  
+    //       alert("Something went wrong")
+  
+    //     }
+    //   }
+    // )
+  }
+
+
+}
